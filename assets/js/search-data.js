5aaf66fccc9d3df148f066cfb68779c9c5287f1d
allyn-loyd <92764557+allyn-loyd@users.noreply.github.com>
2026-03-15 18:31:41 +0000

    Deploying to gh-pages from @ allyn-loyd/allyn-loyd.github.io@64dccc0d214718287bd1559c8f626cdda56e35a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,7 +511,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%6C%6F%79%64@%75%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
