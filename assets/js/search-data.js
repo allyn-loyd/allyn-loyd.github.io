f6191399852ae9e763ab5612e4bd92e2eb5c5886
allyn-loyd <92764557+allyn-loyd@users.noreply.github.com>
2026-07-19 21:50:36 +0000

    Deploying to gh-pages from @ allyn-loyd/allyn-loyd.github.io@cdddf17c3fc09a68340b829c5f33007ed70a2919 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-experience",
           title: "experience",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "An overview of my previous work experience.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/experience/";
